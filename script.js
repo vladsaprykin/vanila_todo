4fb1c1a9f3f78df09f7ec81ba1436e39f6ad5f84
vladsaprykin <saprykin.exceedteam@gmail.com>
2021-08-09 16:00:31 +0300

    code refactoring

--- script.js
+++ script.js
@@ -1,105 +1,80 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    const todoForm = document.querySelector('#todoForm');
-    const radioBtnInputs = [...document.querySelector('#todoForm').querySelector('.todo-form__choose-color').getElementsByClassName('custom-radio')];
+document.addEventListener("DOMContentLoaded", function (event) {
+	const todoForm = document.querySelector('#todoForm');
+	const radioBtnInputs = [...document.getElementsByClassName('custom-radio')];
 
-    radioBtnInputs.forEach(btn => {
-        btn.addEventListener('click', (e) => {
-            clearChecked();
-            e.target.classList.add('checked');
-            e.target.checked = true;
-        })
-    })
-    const clearChecked = () => {
-        radioBtnInputs.forEach( (btn) => {
-            btn.classList.remove('checked');
-            btn.checked = false;
-        })
-    }
-    const colorTodoHandler = (colorStr) => {
-        const arrColors = ['blue', '#ffa400', 'green', 'red', '#00d669', '#530cff'];
-        switch (colorStr){
-            case 'todoColor1' :
-                return 'blue'
-            case 'todoColor2' :
-                return '#ffa400'
-            case 'todoColor3' :
-                return 'green'
-            case 'todoColor4' :
-                return 'red'
-            case 'todoColor5' :
-                return '#00d669'
-            case 'todoColor6' :
-                return '#530cff'
-            default:
-                const randColor = Math.floor(Math.random() * arrColors.length);
-                return arrColors[randColor]
-        }
-    }
-    const getRandColor = () => {
-        clearChecked();
-        const randColor = Math.floor(Math.random() * radioBtnInputs.length);
-        radioBtnInputs[randColor].checked = true;
-        radioBtnInputs[randColor].classList.add('checked');
-    }
-    const checkTodos = () => {
-        const todos = [...document.getElementsByClassName('todo-item')];
+	const colorTodoHandler = (colorStr) => {
+		switch (colorStr) {
+			case 'todoColor1' :
+				return 'blue'
+			case 'todoColor2' :
+				return '#ffa400'
+			case 'todoColor3' :
+				return 'green'
+			case 'todoColor4' :
+				return 'red'
+			case 'todoColor5' :
+				return '#00d669'
+			case 'todoColor6' :
+				return '#530cff'
+		}
+	}
+	const getRandColor = () => {
+		const randColor = Math.floor(Math.random() * radioBtnInputs.length);
+		radioBtnInputs[randColor].checked = true;
+	}
+	const toggleTodo = (todo) => {
+		const isCompleted = todo.getAttribute('completed');
+		const colorItem = todo.getAttribute('color');
+		switch (isCompleted) {
+			case 'true':
+				todo.style.background = '#ccc';
+				todo.querySelector('.todo-item__text').style.textDecoration = 'line-through';
+				break;
+			case 'false':
+				todo.style.background = colorItem;
+				todo.querySelector('.todo-item__text').style.textDecoration = 'none';
+				break;
+		}
+	}
+	const createHandler = (data) => {
+		const divTodo = document.createElement("div");
+		const divCheckBox = document.createElement("div");
+		const checkBoxTodo = document.createElement('input');
+		const textTodo = document.createElement("div");
+		const todoItems = document.querySelector('#todoItems');
 
-        if (!todos.length) return
-        todos.forEach(item => {
-            const completedItem = item.getAttribute('completed');
-            const colorItem = item.getAttribute('color');
+		const divTodoColor = colorTodoHandler(data.todoColor);
 
-            switch (completedItem){
-                case 'true':
-                    item.style.background = '#ccc';
-                    item.querySelector('.todo-item__text').style.textDecoration = 'line-through';
-                    break;
-                case 'false':
-                    item.style.background = colorItem;
-                    item.querySelector('.todo-item__text').style.textDecoration = 'none';
-                    break;
-            }
-        })
-    }
-    const createHandler = (data) => {
-        const divTodo = document.createElement("div");
-        const divCheckBox = document.createElement("div");
-        const checkBoxTodo = document.createElement('input');
-        const textTodo = document.createElement("div");
-        const todoItems = document.querySelector('#todoItems');
+		divTodo.style.background = divTodoColor;
+		divTodo.className = "todo-item";
+		divTodo.setAttribute('completed', 'false');
+		divTodo.setAttribute('color', divTodoColor);
 
-        const divTodoColor = colorTodoHandler(data.todoColor);
+		textTodo.innerText = data.text;
+		textTodo.className = 'todo-item__text';
+		textTodo.style.color = '#ffffff';
 
-        divTodo.style.background = divTodoColor;
-        divTodo.className = "todo-item";
-        divTodo.setAttribute('completed','false');
-        divTodo.setAttribute('color',divTodoColor);
+		divCheckBox.className = 'todo-item__check-box';
+		checkBoxTodo.type = 'checkbox';
 
-        textTodo.innerText = data.text;
-        textTodo.className = 'todo-item__text';
-        textTodo.style.color = '#ffffff';
+		divCheckBox.append(checkBoxTodo);
+		divTodo.append(divCheckBox);
+		divTodo.append(textTodo);
+		todoItems.append(divTodo);
 
-        divCheckBox.className = 'todo-item__check-box';
-        checkBoxTodo.type = 'checkbox';
+		getRandColor();
 
-        divCheckBox.append(checkBoxTodo);
-        divTodo.append(divCheckBox);
-        divTodo.append(textTodo);
-        todoItems.append(divTodo);
-
-        getRandColor();
-
-        checkBoxTodo.addEventListener('change', (e) => {
-            const completedItem = e.target.closest('.todo-item').getAttribute('completed') === 'false' ? 'true' : 'false';
-            e.target.closest('.todo-item').setAttribute('completed', completedItem);
-            checkTodos()
-        })
-    }
-    todoForm.addEventListener('submit', (event) =>{
-        event.preventDefault();
-        const data = Object.fromEntries(new FormData(todoForm).entries());
-        if (!data.text.length) return
-        document.querySelector('.todo__input').value = '';
-        createHandler(data);
-    })
+		checkBoxTodo.addEventListener('change', (e) => {
+			const completedItem = e.target.closest('.todo-item').getAttribute('completed') === 'false' ? 'true' : 'false';
+			e.target.closest('.todo-item').setAttribute('completed', completedItem);
+			toggleTodo(e.target.closest('.todo-item'))
+		})
+	}
+	todoForm.addEventListener('submit', (event) => {
+		event.preventDefault();
+		const data = Object.fromEntries(new FormData(todoForm).entries());
+		if (!data.text.length) return
+		document.querySelector('.todo__input').value = '';
+		createHandler(data);
+	})
 });
