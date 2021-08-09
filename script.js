cc14047291c0e4a2f739d392dba48aacc4587128
vladsaprykin <saprykin.exceedteam@gmail.com>
2021-08-09 17:16:25 +0300

    add localstorage

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 document.addEventListener("DOMContentLoaded", function (event) {
 	const todoForm = document.querySelector('#todoForm');
-	const radioBtnInputs = [...document.getElementsByClassName('custom-radio')];
 
 	const colorTodoHandler = (colorStr) => {
 		switch (colorStr) {
@@ -18,7 +17,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
 				return '#530cff'
 		}
 	}
+
 	const getRandColor = () => {
+		const radioBtnInputs = [...document.getElementsByClassName('custom-radio')];
 		const randColor = Math.floor(Math.random() * radioBtnInputs.length);
 		radioBtnInputs[randColor].checked = true;
 	}
@@ -42,20 +43,22 @@ document.addEventListener("DOMContentLoaded", function (event) {
 		const checkBoxTodo = document.createElement('input');
 		const textTodo = document.createElement("div");
 		const todoItems = document.querySelector('#todoItems');
-
 		const divTodoColor = colorTodoHandler(data.todoColor);
 
-		divTodo.style.background = divTodoColor;
+		divTodo.style.background = (data.isCompleted === 'true') ? '#ccc' : divTodoColor;
 		divTodo.className = "todo-item";
-		divTodo.setAttribute('completed', 'false');
+		divTodo.setAttribute('completed', data.isCompleted);
 		divTodo.setAttribute('color', divTodoColor);
+		divTodo.setAttribute('id',data.id)
 
 		textTodo.innerText = data.text;
 		textTodo.className = 'todo-item__text';
 		textTodo.style.color = '#ffffff';
+		if (data.isCompleted === 'true') textTodo.style.textDecoration = 'line-through';
 
 		divCheckBox.className = 'todo-item__check-box';
 		checkBoxTodo.type = 'checkbox';
+		if (data.isCompleted === 'true') checkBoxTodo.checked = true
 
 		divCheckBox.append(checkBoxTodo);
 		divTodo.append(divCheckBox);
@@ -65,16 +68,40 @@ document.addEventListener("DOMContentLoaded", function (event) {
 		getRandColor();
 
 		checkBoxTodo.addEventListener('change', (e) => {
-			const completedItem = e.target.closest('.todo-item').getAttribute('completed') === 'false' ? 'true' : 'false';
+			const parentBlock = e.target.closest('.todo-item');
+			const completedItem = parentBlock.getAttribute('completed') === 'false' ? 'true' : 'false';
 			e.target.closest('.todo-item').setAttribute('completed', completedItem);
-			toggleTodo(e.target.closest('.todo-item'))
+
+			const todos = JSON.parse(localStorage.getItem("todos"));
+			todos.map( item => {
+				if (item.id === Number(parentBlock.getAttribute('id'))) {
+					item.isCompleted = completedItem;
+					console.log('132')
+				}
+				return item
+			})
+			localStorage.setItem('todos', JSON.stringify(todos));
+			toggleTodo(parentBlock)
 		})
 	}
+	const getTodosLS = () => {
+		const todos = JSON.parse(localStorage.getItem("todos"));
+		todos.forEach( item => createHandler(item))
+	}
+	(localStorage.getItem('todos') === null)  ? localStorage.setItem('todos', JSON.stringify([])) : getTodosLS();
+
 	todoForm.addEventListener('submit', (event) => {
 		event.preventDefault();
 		const data = Object.fromEntries(new FormData(todoForm).entries());
 		if (!data.text.length) return
+		data.isCompleted = 'false';
+		data.id = new Date().getTime();
 		document.querySelector('.todo__input').value = '';
 		createHandler(data);
+
+		const todos = JSON.parse(localStorage.getItem("todos"));
+
+		todos.push(data);
+		localStorage.setItem('todos', JSON.stringify(todos));
 	})
 });
